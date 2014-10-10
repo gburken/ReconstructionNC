5bdc2b3c5492ee83d61eca008f393c32a718a610
ahaak <alex.haak@erasmusmc.nl>
2014-10-10 11:17:59 +0200

    finished first example putting single 2D image into 3D volume

--- putImagesIn3D.cpp
+++ putImagesIn3D.cpp
@@ -1,20 +1,10 @@
-#include "vtkSmartPointer.h"
-#include "vtkPNGReader.h"
-#include "vtkLookupTable.h" // not needed?
-#include <vtkImageMapper3D.h>
-#include <vtkImageMapToColors.h>
-#include "vtkImageActor.h"
-#include "vtkRenderer.h"
-#include "vtkRenderWindowInteractor.h"
-#include "vtkInteractorStyleImage.h"
+#include <itkImage.h>
+#include <itkImageFileReader.h>
+#include <itkEuler3DTransform.h>
+#include <itkResampleImageFilter.h>
+#include <itkImageFileWriter.h>
 
-#include "vtkMatrix4x4.h"
-#include "vtkTransform.h"
-#include "vtkImageViewer.h"
-
-
-
-
+static double pi = 3.14159265359;
 
 int main(int argc, char** argv)
 {
@@ -38,73 +28,86 @@ int main(int argc, char** argv)
     0,-1, 0, 0,
     0, 0, 0, 1 };
 
-  vtkSmartPointer<vtkMatrix4x4> resliceAxes = vtkSmartPointer<vtkMatrix4x4>::New();
-  resliceAxes->DeepCopy(XZPlaneElements);
-  resliceAxes->SetElement(0, 3, 0.0 );resliceAxes->SetElement(1, 3, 0.0 );resliceAxes->SetElement(2, 3, 0.0 );
-  
-  
-  vtkSmartPointer<vtkTransform> resliceAxesTransform = vtkSmartPointer<vtkTransform>::New();
-  resliceAxesTransform->SetMatrix( resliceAxes );
-  resliceAxesTransform->Update();
-  
-
-  resliceAxes->DeepCopy(YZPlaneElements);
-  resliceAxes->SetElement(0, 3, 0.0 );resliceAxes->SetElement(1, 3, 0.0 );resliceAxes->SetElement(2, 3, 0.0 );
-
-  vtkSmartPointer<vtkTransform> resliceAxesTransform2 = vtkSmartPointer<vtkTransform>::New();
-  resliceAxesTransform2->SetMatrix( resliceAxes );
-  resliceAxesTransform2->Update();
+  // fundamental typedefs
+  typedef unsigned char  PixelType;
+  typedef itk::Image<PixelType, 2>  Char2dImageType;
+  typedef itk::Image<PixelType, 3>  Char3dImageType;
+      
 
   // read image
   std::string fileName = "D:/Data/ExperimentalData/EPExperiments/EP1_24-Feb-2014/consecutiveInputDataNC/IM_0007/600Images/2DinputImage_0584.png";
-  vtkSmartPointer<vtkPNGReader> reader = vtkPNGReader::New();
-  reader->SetFileName( fileName.c_str() );
-  reader->Update();
+  typedef itk::ImageFileReader<Char3dImageType> CharReaderType;
+  CharReaderType::Pointer reader = CharReaderType::New();
+  reader->SetFileName( fileName );
+  try
+  {
+    reader->Update();
+  }
+  catch( itk::ExceptionObject & err )
+  {
+    std::cerr << "ExceptionObject caught !" << std::endl;
+    std::cerr << err << std::endl;
+    return EXIT_FAILURE;
+  }
+  Char3dImageType::PointType inOrigin = reader->GetOutput()->GetOrigin();
+  Char3dImageType::SpacingType inSpacing = reader->GetOutput()->GetSpacing();
+  Char3dImageType::SizeType inSize = reader->GetOutput()->GetLargestPossibleRegion().GetSize();
+  inOrigin[0] = (double)(inSize[0]/-2.0)*inSpacing[0];
+  reader->GetOutput()->SetOrigin( inOrigin );
+  reader->GetOutput()->Print( std::cout );
+    
+  // create transform
+  typedef itk::Euler3DTransform<double> TransformTyp;
+  TransformTyp::Pointer transform = TransformTyp::New();
+  TransformTyp::ParametersType transformParameters = transform->GetParameters();
+  transformParameters[0] = 90.0/180.0*pi; transformParameters[1] = 45.0/180.0*pi; transformParameters[2] = 45.0/180.0*pi;
+  std::cout << transformParameters << std::endl;
   
-  // Create a greyscale lookup table
-  vtkSmartPointer<vtkLookupTable> table = vtkSmartPointer<vtkLookupTable>::New();
-  table->SetRange(0, 255); // image intensity range
-  table->SetValueRange(0.0, 1.0); // from black to white
-  table->SetSaturationRange(0.0, 0.0); // no color saturation
-  table->SetRampToLinear();
-  table->Build();
+  // create the output size, spacing etc
+  Char3dImageType::IndexType outIndex;
+  outIndex.Fill( 0 );
+  Char3dImageType::SizeType outSize;
+  outSize.Fill( 600 );
+  Char3dImageType::SpacingType outSpacing;
+  outSpacing.Fill( 1.0 );
+  Char3dImageType::PointType outOrigin;
+  outOrigin.Fill( -300.0 );
 
-  // mapper
-  /*vtkSmartPointer<vtkImageMapper3D> mapper = vtkSmartPointer<vtkImageMapper3D>::New();
-  mapper->SetInputConnection( reader->GetOutputPort() ); 
- */
-  // mapper 2
-  vtkSmartPointer<vtkImageMapToColors> mapper2 = vtkSmartPointer<vtkImageMapToColors>::New();
-  mapper2->SetLookupTable( table );
-  mapper2->SetInputConnection( reader->GetOutputPort() );
-  // actor
-  vtkSmartPointer<vtkImageActor> actor = vtkSmartPointer<vtkImageActor>::New();
-  actor->SetUserTransform( resliceAxesTransform );
-  actor->GetMapper()->SetInputConnection( mapper2->GetOutputPort() );
-  
-  vtkSmartPointer<vtkImageActor> actor2 = vtkSmartPointer<vtkImageActor>::New();
-  actor2->SetUserTransform( resliceAxesTransform2 );
-  actor2->GetMapper()->SetInputConnection( mapper2->GetOutputPort() );
+  // create resample filter
+  typedef itk::ResampleImageFilter<Char3dImageType, Char3dImageType, double> ResamplerType;
+  ResamplerType::Pointer resampler = ResamplerType::New();
+  resampler->SetInput( reader->GetOutput() );
+  resampler->SetTransform( transform );
+  resampler->SetOutputStartIndex( outIndex );
+  resampler->SetSize( outSize );
+  resampler->SetOutputSpacing( outSpacing );
+  resampler->SetOutputOrigin( outOrigin );
+  try
+  {
+    resampler->Update();
+  }
+  catch( itk::ExceptionObject & err )
+  {
+    std::cerr << "ExceptionObject caught !" << std::endl;
+    std::cerr << err << std::endl;
+    return EXIT_FAILURE;
+  }
 
-  // renderer
-  vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
-  renderer->AddActor(actor);
-  renderer->AddActor(actor2);
-  renderer->SetViewport(0.5,0.0,1,0.5);
-
-  //render window
-  vtkSmartPointer<vtkRenderWindow> window = vtkSmartPointer<vtkRenderWindow>::New();
-  window->AddRenderer(renderer);
-
-  // Set up the interaction
-  vtkSmartPointer<vtkInteractorStyleImage> imageStyle = vtkSmartPointer<vtkInteractorStyleImage>::New();
-  vtkSmartPointer<vtkRenderWindowInteractor> interactor =  vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  interactor->SetInteractorStyle( imageStyle );
-  window->SetInteractor( interactor );
-  window->Render();
-  window->Start();
-
-  interactor->Start();
+  // write 3Dimage to file
+  typedef itk::ImageFileWriter<Char3dImageType> WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( resampler->GetOutput() );
+  writer->SetFileName( "outImage.mhd" );
+  try
+  {
+    writer->Update();
+  }
+  catch( itk::ExceptionObject & err )
+  {
+    std::cerr << "ExceptionObject caught !" << std::endl;
+    std::cerr << err << std::endl;
+    return EXIT_FAILURE;
+  }
 
   std::cin.ignore();
   return 0;
